dc6415bd62e96e21b560b016993225dd30cb87d2
Sai Arjun Shroff <66201760+arjun-71@users.noreply.github.com>
2023-10-10 13:12:55 -0700

    reforming the code and adding dictionary mapping

---
+++ HelperMethods/InformationToObject.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import HelperMethods.DatabaseString as dbs
+import HelperMethods.ExcelToCsv as converter
+import HelperMethods.csv_name_changer as nameChanger
+import dataManagement.hash_function
+import dataManagement.dataBody as dsn 
+import dataManagement.dataBody
+import HelperMethods.InformationToObject as objectInserter
+
+
+def process_csv_data(new_csv_data):
+    codekey_one = ""
+    project_name = ""
+    section_value = ""
+    sub_section_value = ""
+    project_code_value = ""
+    budgetData = dataManagement.dataBody.BudgetData()  # Initialize an empty DataFrame for budgetData
+
+    for index, row in new_csv_data.iterrows():
+        project_code_value = new_csv_data.columns[0]
+
+        for column_name, value in row.items():
+            if index == 0 and value == "Current Budget":
+                project_name = column_name
+            if column_name == "Unnamed: 1" and value != "0" and pd.notna(value) and index > 1:
+                section_value = value
+
+            if pd.notna(value) and index > 3 and index < 38:
+                column_field_value = budgetData.replace_value(column_name)
+                if column_name == project_code_value:
+                    sub_section_value = value
+                if column_field_value in ['Encumbered', 'Expensed', 'Anticipated Costs', 'Uncommitted Budget', 'Current Budget', 'At Construction Budget'] and sub_section_value.find("Subtotal") == -1:
+                    budgetData.set_value([project_name, section_value, sub_section_value, column_field_value], value)
+
+    return budgetData
